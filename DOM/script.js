175e08c0070481fc8f366d859e3df4cbb1a23805
aaronkipkirui <aaronkipkirui015@gmail.com>
2026-03-04 10:55:32 -0400

    event listeners in js

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,4 @@
+// add event listener to the add button
 document.getElementById("myButton").addEventListener("click", function(){
     var itemInput = document.getElementById("itemInput");
     var itemText = itemInput.value.trim();
@@ -9,3 +10,20 @@ document.getElementById("myButton").addEventListener("click", function(){
         itemInput.value = "";
     }
 });
+
+// add event listener to the clear button
+document.getElementById("clearButton").addEventListener("click", function(){
+    var ul = document.querySelector("ul");
+    var li = document.querySelector("li");
+    if (ul.innerHTML !== "") {
+        ul.removeChild(ul.lastChild);
+    }
+})
+
+// add event listener for enter key press in the input field
+document.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("myButton").click();
+    }
+})
+
